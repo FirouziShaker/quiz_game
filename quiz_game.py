35a31f91cab9532c6463a3c51212c4a9a171a23b
FirozuiShaker <furouzi.abolfazl@yahoo.com>
2023-10-06 19:19:45 +0200

    if and else process

---
+++ quiz_game.py
@@ -0,0 +1,14 @@
+print(" welcome to my computer quiz!")
+
+playing=input("Do you want to play? ")
+
+if playing !="yes":
+    quit()
+    
+print("Okay! Let's play:)")
+
+answer =input("what does CPU stand for?")
+if answer=="central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect")
